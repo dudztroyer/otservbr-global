6d6731c680c1551e545685478725c903d01aa4f7
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-03-22 00:45:47 -0300

    Update protocolgame.cpp

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -1377,7 +1377,7 @@ void ProtocolGame::sendCreatureType(uint32_t creatureId, uint8_t creatureType)
 	}
 
 	if (creatureType == CREATURETYPE_SUMMON_OWN && player->getProtocolVersion() >= 1120) {
-		const Creature* master = creatureId->getMaster();
+		uint32_t master = creatureId->getMaster();
 		if (master) {
 			msg.add<uint32_t>(master->getID());
 		}
